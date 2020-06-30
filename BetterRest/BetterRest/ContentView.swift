404ff420e295fe46ab21b84175ece5c4962bd302
Luis Eduardo Gonzalez <luiseddglez@gmail.com>
2020-06-29 23:01:42 -0400

    Day 26

---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Luis Eduardo Gonzalez on 2020-06-29.
+//  Copyright © 2020 Luis Eduardo Gonzalez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
