5c6e7e0a51566706c6614757cadf2ba12a785104
Luis Eduardo Gonzalez <luiseddglez@gmail.com>
2020-07-02 08:50:01 -0400

    Day 27

---
+++ RockPaperScissors/RockPaperScissors/Paths.swift
@@ -0,0 +1,26 @@
+//
+//  Paths.swift
+//  RockPaperScissors
+//
+//  Created by Luis Eduardo Gonzalez on 2020-06-30.
+//  Copyright © 2020 Luis Eduardo Gonzalez. All rights reserved.
+//
+
+import SwiftUI
+
+struct Paths: View {
+    var body: some View {
+        Text("Blah")
+        .background(
+            Image("login-background")
+        )
+    }
+}
+
+struct Paths_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            Paths()
+        }
+    }
+}
