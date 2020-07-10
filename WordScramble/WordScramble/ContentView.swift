d84db9e656c3b7850cf08c910781765021e11bee
Luis Eduardo Gonzalez <luiseddglez@gmail.com>
2020-07-10 08:56:47 -0400

    Day 29

---
+++ WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Luis Eduardo Gonzalez on 2020-07-10.
+//  Copyright © 2020 Luis Eduardo Gonzalez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
